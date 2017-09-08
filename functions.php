98c17fcc15cbc1472a5a2a8b82cd83dfa36e8ab1
Valerie Gayle <31604787+fdc-valerie@users.noreply.github.com>
2017-09-08 10:32:31 -0700

    Done with PDO CLASS

--- functions.php
+++ functions.php
@@ -1,11 +1,26 @@
 <?php 
 include('phpact.php');
 
-$pdo = PracticeOOP::dbConnect();
+// $pdo = PracticeOOP::dbConnect();
+
+$pdo = new PracticeOOP();
 
 if($pdo){
-echo 'Connect Successfull';
+echo 'Connect Successfull <br>';
+	
+	$pdo->createDB();
+	$pdo->createTable();
+	$pdo->insertRow();
+	$pdo->viewRow();
+	$pdo->updateData();
+	$pdo->deleteData();
+
 }else{
-	echo 'errpr';
+	echo 'error';
 }
+	// $sql = "CREATE DATABASE IF NOT EXISTS Facuadsadlty";
+ //    $pdo->exec($sql);
+ //     echo "DB created successfully";
+ //     echo "<br>";
+    
 ?>
--- phpact.php
+++ phpact.php
@@ -13,29 +13,120 @@ class PracticeOOP{
 	public function dbConnect(){
 		if(null == self::$conn){
 			try{
-	   			self::$conn = new PDO("mysql:host=".self::$servername.";dbname=".self::$dbname.";", self::$username, self::$password);
+	   			$x = new PDO("mysql:host=".self::$servername.";dbname=".self::$dbname.";", self::$username, self::$password);
 	   		// self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		    }
 			catch(PDOException $e){
 		    	echo "Connection failed: " . $e->getMessage();
 		    }	
 		}
-		return self::$conn;
-
-	
+		return $x;
+		
 	}
 
 	public function createDB(){
+		$sql = "CREATE DATABASE IF NOT EXISTS Faculty";
+		$this->pdo->exec($sql);
+		echo "DB created successfully";
+		echo "<br>";
+	}
 
-
-	$sql = "CREATE DATABASE IF NOT EXISTS Faculty";
-    $this->pdo->exec($sql);
-     echo "DB created successfully";
-     echo "<br>";
-     createTable();
+	public function createTable(){
+		$sql = "USE Faculty";
+    	$this->pdo->exec($sql);
+    	$sql = "CREATE TABLE IF NOT EXISTS TEACHER (
+              	ID int(11) AUTO_INCREMENT PRIMARY KEY,
+              	first_name varchar(30)NOT NULL,
+                last_name varchar(30) NOT NULL,
+                time_in TIMESTAMP,
+                type enum('FULL-TIME','PART-TIME'))";
+    	$this->pdo->exec($sql);
+    	echo "Table created succesfully";
+   	 	echo "<br>";
+  
+}
+	public function insertRow(){
+		$sql = "SELECT * from TEACHER   ";
+		$stmt = $this->pdo->prepare($sql);
+		$sql= "INSERT INTO TEACHER (ID, first_name, last_name, time_in, type)
+		VALUES (NULL, 'Doesssaa', 'jsfasfas', now(), 'FULL-TIME')";
+		$this->pdo->exec($sql);
+		echo "New data added succesfully";
+		echo "<br>";
+		
 }
 
+	public function viewRow(){
+	$sql = "SELECT * from TEACHER  where flag=1 ";
+	$stmt = $this->pdo->prepare($sql);
+	$stmt->execute(array());
+	$result = $stmt -> fetchAll();
+
+		echo'<h3> LIST ALL THE ROWS OF A TABLE
+	 </h3>';
+		echo "<table>
+		<thead>
+		<tr>
+		<th>ID</th>
+		<th>First Name</th>
+		<th>Last Name</th>
+		<th>Time In </th>
+		<th> Type </th>
+		</tr>"; 
+
+	foreach( $result as $row ) {
+		echo "<tr>";
+		echo "<td>" .$row['ID']. "</td>";
+	    echo "<td>" .$row['first_name']. "</td>";
+	    echo "<td>" .$row['last_name']. "</td>" ;
+	    echo "<td>" .$row['time_in']. "</td>" ;
+		echo "<td>" .$row['type']. "</td>" ;
+	}
+
+	echo "</table>";
+
 }
+	public function viewOneData(){
+ 	$sql = "SELECT * from TEACHER LIMIT 1";
+	$stmt = $this->pdo->prepare($sql);
+	$stmt->execute(array());
+	$result = $stmt ->fetch(PDO::FETCH_ASSOC);
+		echo'<br/>';
+		echo'<h3> LIST 1 Row of table </h3>';
+		echo '<p>' . $result["ID"] . ' ' . $result["first_name"]  . '   ' . $result["last_name"]  .  '    ' . $result["time_in"]  .  '    ' .  $result["type"] .  '</p>';   
+		echo '<br/>';
+
+}
+	public function updateData(){
+		echo '<h3> Update ID #3 Information to Valerie Lomocso </h3>';	
+		$sql ="UPDATE TEACHER SET first_name='Valerie',last_name='Lomocso' WHERE ID=3";
+		$this->pdo->exec($sql);
+		echo "Data has been updated";
+		echo '<br><br>';
+
+}
+
+	public function deleteData()
+{
+	echo'Delete ID #10';
+	// $sql ="DELETE FROM TEACHER WHERE ID=10";
+	$sql ="UPDATE TEACHER SET flag=0 where ID=10";
+	$this->pdo->exec($sql);
+	echo "  Data has been deleted";
+	echo '<br';
+
+}
+	// private function closeConnection() {
+	// 	$this->pdo = null;
+	// }
+
+}
+
+/**
+* 
+*/
+
+
 
 
 	
